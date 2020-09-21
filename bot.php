5e312e87e721a666cd106e9b05cdc34cc7ed5f90
Andrysha3452 <70117145+Andrysha3452@users.noreply.github.com>
2020-09-22 00:55:49 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,33 +1,8 @@
 <?php
-
-include "vk_api.php"; 
-
-
-const VK_KEY = "edcea0872f2c137a578c61d490abc2156657ca12ba0d12b1e95b3cb62025e3a4bd61aef8a94da85123d58";  // Токен сообщества
-const ACCESS_KEY = "9b5a43be";  // Тот самый ключ из сообщества 
-const VERSION = "5.81"; // Версия API VK
-
-
-$vk = new vk_api(VK_KEY, VERSION); 
-$data = json_decode(file_get_contents('php://input')); 
-
-if ($data->type == 'confirmation') { 
-    exit(ACCESS_KEY); 
+class cinfigBotVK
+{
+	public $key = "5186ece87fa496a3fdcb5a00f23b48f5939d5c6a7317da15062ef8183b2ac14e0f900ae9828c415d85a59";
+	public $privat = "ebanashka";
+	public $back = "d5b25c5a";
 }
-$vk->sendOK(); 
-// ====== Наши переменные ============
-$id = $data->object->from_id; // Узнаем ID пользователя, кто написал нам
-$message = $data->object->text; // Само сообщение от пользователя
-// ====== *************** ============
-
-if ($data->type == 'message_new') {
-
-    if ($message == '!бот') {
-
-            $vk->sendMessage($id, "Привет :-)");
-			
-        }
-
-
-	}
-	
+?>
